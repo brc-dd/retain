e896a8c518ba527033ec670f1aada9453b320f1c
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2025-05-28 19:56:28 +0530

    fix: make paths relative to homedir

--- main.ts
+++ main.ts
@@ -3,42 +3,20 @@
 import $ from 'jsr:@david/dax'
 import { expandGlob } from 'jsr:@std/fs'
 
-const root = Deno.env.get('HOME')
-if (!root) {
-  console.error('HOME environment variable is not set.')
-  Deno.exit(1)
-}
+const root = Deno.env.get('HOME')!
 
-const list = (await Deno.readTextFile('./include.txt'))
-  .split('\n')
-  .map((_) => _.trim())
+const list = (await Deno.readTextFile('./include.txt')).split('\n').map((_) => _.trim())
   .filter(Boolean)
-  .map((file) => `${root}/${file}`)
 
-const exclude = (await Deno.readTextFile('./exclude.txt'))
-  .split('\n')
-  .map((_) => _.trim())
+const exclude = (await Deno.readTextFile('./exclude.txt')).split('\n').map((_) => _.trim())
   .filter(Boolean)
 
 await $`brew bundle dump --global --force`
 
-for await (const e of expandGlob('**/*', {
-  root,
-  includeDirs: false,
-  followSymlinks: false,
-  exclude
-}))
-  e.isFile && list.push(e.path)
+for await (
+  const e of expandGlob('**/*', { root, includeDirs: false, followSymlinks: false, exclude })
+) e.isFile && list.push(e.path.slice(root.length + 1))
 
-try {
-  await Deno.mkdir(`${root}/.retain`, { recursive: true })
-} catch (error) {
-  if (!(error instanceof Deno.errors.AlreadyExists)) {
-    console.error('Error creating .retain directory:', error)
-    Deno.exit(1)
-  }
-}
-
-await Deno.writeTextFile(`${root}/.retain/backup.txt`, list.join('\n'))
+await Deno.writeTextFile('backup.txt', list.join('\n'))
 await $`cd ~; zip -r .retain/backup.zip -@ < .retain/backup.txt`.noThrow(18, 12)
 await $`cd ~; cp .retain/backup.zip "$HOME/Library/Mobile Documents/com~apple~CloudDocs/backup.zip"`
