777b20648d61bb889b9f0c68a6acf20bce3f7615
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2025-05-29 14:18:00 +0530

    chore: minor tweaks

--- Casks/retain.rb
+++ Casks/retain.rb
@@ -1,20 +1,20 @@
 cask "retain" do
-  version "20250529135843"
+  version "20250529141707"
   sha256 "d970c92e6ba6c55968825514703fbcd411146edd8d704c6f5c81ac8a25bc9365"
 
   url "https://raw.githubusercontent.com/brc-dd/retain/refs/heads/main/Retain-d970c92e.zip"
   name "Retain"
-  desc "Automated nightly iCloud-Drive backup"
+  desc "Automated nightly iCloud drive backup"
   homepage "https://github.com/brc-dd/retain"
 
-  depends_on formula: ["fd", "mas"]
+  depends_on formula: %w[fd mas]
 
   app "Retain.app"
 
-  plist_path = File.expand_path("~/Library/LaunchAgents/dev.brc-dd.retain.plist")
-  app_path = "#{appdir}/Retain.app"
-
   postflight do
+    plist_path = File.expand_path("~/Library/LaunchAgents/dev.brc-dd.retain.plist")
+    app_path = "#{appdir}/Retain.app"
+
     ohai "Running Retain once to prompt for permissions..."
     system_command "/usr/bin/open", args: ["-a", app_path]
 
